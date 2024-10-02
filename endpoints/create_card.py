73082e1794c9fc89b14eb88946b4d4d613934475
kubanemil <emil3toktobekov@gmail.com>
2024-10-02 10:14:49 +0500

    seperate tests

--- endpoints/create_card.py
+++ endpoints/create_card.py
@@ -2,7 +2,6 @@ import json
 import logging
 import uuid
 
-import pytest
 from models import Card
 
 logger = logging.getLogger()
@@ -49,75 +48,3 @@ def lambda_handler(event: dict[str, any], context: any) -> dict[str, any]:
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"error": str(e)}),
         }
-
-
-@pytest.fixture
-def test_event():
-    return {
-        "resource": "/",
-        "path": "/",
-        "httpMethod": "POST",
-        "queryStringParameters": None,
-        "multiValueQueryStringParameters": None,
-        "pathParameters": None,
-        "stageVariables": None,
-        "requestContext": {
-            "resourceId": "z5arjv47tl",
-            "resourcePath": "/",
-            "operationName": "createCard",
-            "httpMethod": "POST",
-            "extendedRequestId": "e6pB1GKFBcwEL-Q=",
-            "requestTime": "30/Sep/2024:11:16:01 +0000",
-            "path": "/dev",
-            "accountId": "010046428417",
-            "protocol": "HTTP/1.1",
-            "stage": "dev",
-            "domainPrefix": "yk5bfx14nh",
-            "requestTimeEpoch": 1727694961854,
-            "requestId": "04b2c83e-2a5d-4be3-89f3-5d33e9d273cf",
-            "domainName": "yk5bfx14nh.execute-api.ap-south-1.amazonaws.com",
-            "deploymentId": "qvmk14",
-            "apiId": "yk5bfx14nh",
-        },
-        "body": '{\r\n    "name": "Gojo",\r\n    "image": "https://static.wikia.nocookie.net/jujutsu-kaisen/images/e/ef/Satoru_Gojo_%28Anime_2%29.png/revision/latest?cb=20240622022211",\r\n    "attack": 100,\r\n    "defense": 100,\r\n    "ability": "Inifinte void",\r\n    "grade": "Special",\r\n    "reach": "Melee"\r\n}',
-        "isBase64Encoded": False,
-    }
-
-
-def test_get_card_success(test_event):
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 200, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert body.keys() == set(
-        ["id", "ability", "image", "name", "attack", "defense", "grade", "reach"]
-    )
-
-
-def test_get_card_key_error(test_event):
-    body: dict = json.loads(test_event["body"])
-    del body["image"]
-    test_event["body"] = json.dumps(body)
-    logger.info(test_event["body"])
-
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 400, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert "Missing required field:" in body["error"]
-
-
-def test_get_card_internal_error(test_event):
-    body: dict = json.loads(test_event["body"])
-    body["grade"] = "Fourth"
-    test_event["body"] = json.dumps(body)
-    logger.info(test_event["body"])
-
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 500, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert "error" in body
--- endpoints/get_card.py
+++ endpoints/get_card.py
@@ -1,7 +1,6 @@
 import json
 import logging
 
-import pytest
 from models import Card
 
 logger = logging.getLogger()
@@ -39,69 +38,3 @@ def lambda_handler(event: dict[str, any], context: any) -> dict[str, any]:
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"error": str(e)}),
         }
-
-
-@pytest.fixture
-def test_event():
-    return {
-        "resource": "/",
-        "path": "/",
-        "httpMethod": "GET",
-        "queryStringParameters": {"card_id": "123"},
-        "multiValueQueryStringParameters": {"card_id": ["123"]},
-        "pathParameters": None,
-        "stageVariables": None,
-        "requestContext": {
-            "resourceId": "z5arjv47tl",
-            "resourcePath": "/",
-            "operationName": "getCards",
-            "httpMethod": "GET",
-            "extendedRequestId": "e6bsLFbJBcwEMsA=",
-            "requestTime": "30/Sep/2024:09:44:58 +0000",
-            "path": "/dev",
-            "accountId": "010046428417",
-            "protocol": "HTTP/1.1",
-            "stage": "dev",
-            "domainPrefix": "yk5bfx14nh",
-            "requestTimeEpoch": 1727689498453,
-            "requestId": "4809643d-6e83-414d-820f-6e59ffb1043d",
-            "domainName": "yk5bfx14nh.execute-api.ap-south-1.amazonaws.com",
-            "deploymentId": "ps9dx4",
-            "apiId": "yk5bfx14nh",
-        },
-        "body": None,
-        "isBase64Encoded": False,
-    }
-
-
-def test_get_card_success(test_event):
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 200, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert body.keys() == set(
-        ["id", "ability", "image", "name", "attack", "defense", "grade", "reach"]
-    )
-
-
-def test_get_card_not_found(test_event):
-    test_event["queryStringParameters"] = {"card_id": "666"}
-
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 404, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert body == {"error": "Card not found"}
-
-
-def test_get_card_internal_error(test_event):
-    del test_event["queryStringParameters"]["card_id"]
-
-    response = lambda_handler(test_event, None)
-    assert response["statusCode"] == 500, "Wrong status code"
-    assert response["headers"] == {"Content-Type": "application/json"}, "Wrong headers"
-
-    body: dict = json.loads(response["body"])
-    assert "error" in body
